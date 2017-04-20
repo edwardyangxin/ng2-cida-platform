ddc838d2042504d531a732b3d7422c9c3004cdfc
edwardyangxin <yx_yoyo@live.cn>
2017-04-20 16:02:35 +0800

    Create portal.component.ts

---
+++ portal.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    template: '<router-outlet></router-outlet>'
+})
+export class PortalComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
