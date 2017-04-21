41668f9143f4716f9fd488487f4428925b343913
edwardyangxin <yx_yoyo@live.cn>
2017-04-21 17:20:52 +0800

    Update portal.component.ts

--- portal.component.ts
+++ portal.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiService, commonConfig} from '../../frameworks';
+import {appCommon} from '../config/app-common';
 
 @Component({
     moduleId: module.id,
     template: '<router-outlet></router-outlet>'
 })
 export class PortalComponent implements OnInit {
-    constructor() { }
+    constructor(api: ApiService) {
+        api.baseUrl = `${commonConfig.baseUrl}${appCommon.appName}`;
+    }
 
-    ngOnInit() { }
+    ngOnInit() {
+        console.log("starting init portal comp....======================")
+    }
 
 }
