211f0edb07ed85bb5d5cc2a5af00643cbb83f238
edwardyangxin <yx_yoyo@live.cn>
2017-04-21 17:19:42 +0800

    Update portal.routing.ts

--- portal.routing.ts
+++ portal.routing.ts
@@ -1,24 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { PortalComponent } from './portal.component';
-import { TestComponent }    from './home/test/test.component';
-import { Test1Component }   from './home/test1/test1.component';
-import { Home1sComponent }   from './home/home1/home1.component';
-import { Home2sComponent }   from './home/home2/home2.component';
+import {NgModule} from "@angular/core";
+import {Routes, RouterModule} from "@angular/router";
+import {PortalComponent} from "./portal.component";
+
 export const routes: Routes = [
-  { path: '',
+  {
+    path: '',
     component: PortalComponent,
     children: [
-      { path: 'caseHome', component: HomeComponent },
-      
-      { path: '', pathMatch: 'full', redirectTo: 'caseHome' },
-      { path: 'test', component: TestComponent },
-      { path: 'test1', component: Test1Component },
-      { path: 'home1', component: Home1sComponent },
-      { path: 'home2', component: Home2sComponent }
+      { path: '', redirectTo: 'testcase', pathMatch: 'full' },
+      { path: 'testcase', loadChildren: './home/home.module#TestcaseHomeModule' }
     ]
-  },
+  }
 ];
 
 @NgModule({
