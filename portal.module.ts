e44f0b5aefb49cd374793aafa2120e73ef319af5
edwardyangxin <yx_yoyo@live.cn>
2017-04-21 17:20:27 +0800

    Update portal.module.ts

--- portal.module.ts
+++ portal.module.ts
@@ -1,29 +1,17 @@
-import { NgModule }                             from '@angular/core';
-import { PortalRoutingModule }                  from './portal.routing';
-import { PortalSharedModule }                   from './shared/portal-shared.module';
-import { PortalComponent }                      from './portal.component';
-import { HomeComponent }                        from './home/home.component';
-import { TestComponent }                      from './home/test/test.component';
-import { Test1Component }                      from './home/test1/test1.component';
-import {CaseHomeService}                        from "./services/CaseHomeService";
-import { Home1sComponent }                      from './home/home1/home1.component';
-import { Home2sComponent }                      from './home/home2/home2.component';
+import {NgModule} from "@angular/core";
+import {PortalRoutingModule} from "./portal.routing";
+import {PortalSharedModule} from "./shared/portal-shared.module";
+import {PortalComponent} from "./portal.component";
 @NgModule({
     imports: [
-      PortalSharedModule,
-      PortalRoutingModule
-    ], 
+        PortalSharedModule,
+        PortalRoutingModule
+    ],
     declarations: [
-      PortalComponent,
-      HomeComponent,
-      TestComponent,
-      Test1Component,
-      Home1sComponent,
-      Home2sComponent
+        PortalComponent
     ],
     providers: [
-      CaseHomeService
-
     ],
 })
-export class PortalModule { }
+export class PortalModule {
+}
